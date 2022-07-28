fa69ef69aea593e7b915f94fc413a961d9a3b6e2
ChrixWX92 <chris@wards-net.co.uk>
2022-07-28 13:36:58 +0100

    Initial upload

--- src/main/java/com/gms/paper/commands/Pets.java
+++ src/main/java/com/gms/paper/commands/Pets.java
@@ -24,7 +24,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 
-import static com.gms.paper.forms.PetsForm.*;
+import static com.gms.paper.forms.PuzzleForm.*;
 
 public class Pets extends Command {
 
